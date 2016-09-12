933fac30834798ccb81aa3f3cf02ba0e43f6f4e3
PayAsian <ptre8426@csddocs.org>
2016-09-12 13:19:37 -0600

    I added more questions and i also learned about .Int and .Double which are two ways of incorporating numbers into the code and inputing numbers

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -25,6 +25,36 @@ public class StupidController
 		System.out.println("What is your Name?");
 		String userInput = readKeyboardInput.nextLine();
 		System.out.println("You said: " + userInput); 
+	
+		System.out.println(" What is your favorit color? ");
+		String userInput1 = readKeyboardInput.nextLine();
+		System.out.println(" That's cool I also love the color " + userInput1);
+	
+		System.out.println(" What is your favorite type of food? ");
+		String userInput2 = readKeyboardInput.nextLine();
+		System.out.println("No Way! I love " + userInput2);
+	
+		System.out.println(" What is your faviorte Video Game of all time? ");
+		String userInput3 = readKeyboardInput.nextLine();
+		System.out.println(" NO way you play it too, ive never met anyone else who have played " + userInput3);
+	
+		System.out.println("When is your birthday? ");
+		int userInput4 = readKeyboardInput.nextInt();
+		System.out.println("I love the number" + userInput4);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }
